60fb705c8e0f76e561794a62ec424a9291b4c6b8
Noah Broyles <noah@javamate.net>
2019-11-07 21:27:33 -0500

    Changed Chat App.py to reflect the file structrue changes.

--- SimpleChatClient/Chat App.py
+++ SimpleChatClient/Chat App.py
@@ -1,7 +1,5 @@
 import os, sys, time, getpass
 print()
-# This software is © 2019 by Noah Broyles. All Rights Reserved.
-# Chat App version 3
 
 # isVaild(ip) is not used in this program
 def isValidIp(ip):
@@ -28,8 +26,8 @@ try:
     server = input("Would you like to:\n[1] Host the Chat Server on your computer\n[2] or connect to the Chat Server server on a different computer? ")
     print()
     if server[0] == '1':
-        print('Okay. You are the host. To see what people are sending, run: \'java SimpleChatClient 127.0.0.1 username\'')
-        os.system("cd java/;java VerySimpleChatServer")
+        print('Okay. You are the host. To see what people are sending, run: \'cd classes; java SimpleChatClient 127.0.0.1 username\'')
+        os.system("cd classes/;java VerySimpleChatServer")
     elif server[0] == '2':
         ip = getIp()
         userName = input('What do you want to be you user name? Spaces are not allowed: ')
@@ -39,6 +37,6 @@ try:
             print('Hey man, just so you know, your username can\'t have spaces in it. So don\'t freak out when it isn\'t the same as you said.')
         print()
         print('Connecting to the chat server @',ip,'using', userName,'as the User Name. Please Wait...')
-        os.system("cd java/;java SimpleChatClient " + ip + " "+ userName)
+        os.system("cd classes/;java SimpleChatClient " + ip + " "+ userName)
 except KeyboardInterrupt:
     sys.exit()
