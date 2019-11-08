d5ce7eaa0e27d0677ed06efaef7aeaccdbd8bfdf
Noah Broyles <noah@javamate.net>
2019-11-07 21:42:14 -0500

    Done it!

--- SimpleChatClient/RUNFIRST.py
+++ SimpleChatClient/RUNFIRST.py
@@ -8,4 +8,5 @@ class Validate:
 if Validate.isWindows():
     os.system("cd source && javac -d ../classes/. *.java && cd ../classes && jar -cvmf manifest.txt SimpleChatClient.jar *")
 else:
+    # Now I know that this works the other way, but better safe than sorry
     os.system("cd source; javac -d ../classes/. *.java; cd ../classes; jar -cvmf manifest.txt SimpleChatClient.jar *")
