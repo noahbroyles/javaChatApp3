2409d76c2638ccc5c299006e60f4a7e88aacaa76
Noah Broyles <noah@javamate.net>
2019-11-07 21:39:22 -0500

    Trying to add Windows vs Unix support

--- SimpleChatClient/RUNFIRST.py
+++ SimpleChatClient/RUNFIRST.py
@@ -1,2 +1,11 @@
 import os
-os.system("cd source; javac -d ../classes/. *.java; cd ../classes; jar -cvmf manifest.txt SimpleChatClient.jar *")
+
+class Validate:
+    def isWindows():
+        if os.path.exists("C:"):
+            return True
+
+if Validate.isWindows():
+    os.system("cd source && javac -d ../classes/. *.java && cd ../classes && jar -cvmf manifest.txt SimpleChatClient.jar *")
+else:
+    os.system("cd source; javac -d ../classes/. *.java; cd ../classes; jar -cvmf manifest.txt SimpleChatClient.jar *")
